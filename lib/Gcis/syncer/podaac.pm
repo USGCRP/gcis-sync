a133ec092c92da4b225f1338dd51592bfb2ca4bf
Brian Duggan <bduggan@usgcrp.gov>
2014-07-25 11:14:49 -0700

    platforms, sensors

--- lib/Gcis/syncer/podaac.pm
+++ lib/Gcis/syncer/podaac.pm
@@ -1,18 +1,22 @@
 package Gcis::syncer::podaac;
-use Gcis::Client;
 use base 'Gcis::syncer';
-use Gcis::syncer::util qw/:log iso_date/;
+
+use Gcis::Client;
+use Gcis::syncer::util qw/:log iso_date pretty_id/;
 use Smart::Comments;
 use Mojo::UserAgent;
 use Data::Dumper;
 
 use v5.14;
 our $src = "http://podaac.jpl.nasa.gov/ws/search/dataset/";
+our $meta_src = "http://podaac.jpl.nasa.gov/ws/metadata/dataset/"; # datasetId => PODAAC-TPRGD-NET10, format => gcmd
+my $ua  = Mojo::UserAgent->new();
+
 our $map = {
-    identifier  =>  sub { my $dom = shift; "nasa-".(lc $dom->id->text); },
+    identifier  =>  sub { my $dom = shift; "nasa-podaac-".(lc $dom->id->text); },
     name        =>  sub { my $dom = shift; $dom->title->text;         },
-    description =>  sub { my $dom = shift; $dom->at('content')->text; },
-    native_id   =>  sub { my $dom = shift; $dom->shortName->text;     },
+    description =>  sub { my $dom = shift; join "\n", $dom->at('content')->text, "short name : ".$dom->shortName->text;  },
+    native_id   =>  sub { my $dom = shift; $dom->datasetId->text;     },
     url         =>  sub { my $dom = shift; $dom->at('link[title="Dataset Information"]')->attr('href'); },
     lon_min     =>  sub { my $dom = shift; my $w = $dom->at('where') or return undef;
                                            [ split / /, $w->Envelope->lowerCorner->text ]->[0]; },
@@ -27,45 +31,76 @@ our $map = {
     release_dt  =>  sub { iso_date(shift->updated->text);                                                },
 };
 
+our $rel = [
+    [
+      platforms => sub {
+        my $dataset = shift;
+        my $dif = shift;
+        for ($dif->find("Source_Name > Short_Name")->each) {
+          my $platform_identifier = pretty_id($_->text);
+              debug "dataset ".$dataset->{identifier}." has platform $platform_identifier";
+        }
+      }
+    ],
+    [
+      sensors => sub {
+        my $dataset = shift;
+        my $dif = shift;
+        for ($dif->find("Sensor_Name > Short_Name")->each) {
+          my $sensor_identifier = pretty_id($_->text);
+          debug "dataset ".$dataset->{identifier}." has sensor $sensor_identifier";
+        }
+      },
+    ],
+];
+
 sub sync {
     my $s = shift;
     my %a = @_;
     my $limit   = $a{limit};
     my $dry_run = $a{dry_run};
     my $gcid    = $a{gcid};
-    my $c = $s->{gcis} or die "no client";
+    my $c       = $s->{gcis} or die "no client";
     return if ($gcid && $gcid !~ /^\/dataset\/nasa-podaac-/);
     my %stats;
 
     my $per_page    = 400;
     my $more        = 1;
     my $start_index = 1;
-    my $ua          = Mojo::UserAgent->new();
     my $url         = Mojo::URL->new($src)->query(format => "atom", itemsPerPage => $per_page);
+    my $count       = 1;
 
+    REQUEST :
     while ($more) {
         $more = 0;
         my $tx = $ua->get($url->query([ startIndex => $start_index ]));
         my $res = $tx->success or die $tx->error;
         for my $entry ($res->dom->find('entry')->each) {  ### Processing===[%]       done
-            my %gcis_info = $s->_extract_gcis($entry);
+            last REQUEST if $limit && $count > $limit;
             $more = 1;
+            my %gcis_info = $s->_extract_gcis($entry);
             next if $gcid && $gcid ne "/dataset/$gcis_info{identifier}";
+            debug "entry #$count : $gcis_info{identifier}";
+            $count++;
 
             # insert or update
             my $existing = $c->get("/dataset/$gcis_info{identifier}");
             my $url = $existing ? "/dataset/$gcis_info{identifier}" : "/dataset";
             $stats{ ($existing ? "updated" : "created") }++;
-            if ($dry_run) {
-                info "ready to POST to $url";
-                next;
-            }
             # TODO skip if unchanged
             debug "sending ".Dumper(\%gcis_info);
-            $c->post($url => \%gcis_info) or do {
-                error $c->error;
-                die "bailing out, error : ".$c->error;
-            };
+            unless ($dry_run) {
+                $c->post($url => \%gcis_info) or do {
+                    error $c->error;
+                    die "bailing out, error : ".$c->error;
+                };
+            }
+            my $dif = $s->_retrieve_dataset_meta($gcis_info{native_id});
+            for my $rel (@$rel) {
+                my ($name,$code) = @$rel;
+                debug "updating $name";
+                $code->(\%gcis_info, $dif);
+            }
         }
         $start_index += $per_page;
     }
@@ -74,15 +109,23 @@ sub sync {
     return;
 }
 
+sub _retrieve_dataset_meta {
+    my $s = shift;
+    my $id = shift;
+    my $url = Mojo::URL->new($meta_src)->query(datasetId => $id, format => 'gcmd');
+    debug "getting $url";
+    my $tx = $ua->get($url);
+    my $res = $tx->success or die $tx->error;
+    return $res->dom;
+}
+
 sub _extract_gcis {
-    state $count;
     my $s = shift;
     my $dom = shift;
     our $map;
 
     my %new = map { $_ => $map->{$_}->( $dom ) } keys %$map;
-    $count++;
-    debug "extracting entry $count : $new{identifier} : $new{native_id}";
+    debug "extracting $new{identifier} : $new{native_id}";
     return %new;
 }
 
