fbd47ac53ce0e2888865f286c6310fb5f0423841
Brian Duggan <bduggan@usgcrp.gov>
2014-07-25 11:14:05 -0700

    prettier id

--- lib/Gcis/syncer/util.pm
+++ lib/Gcis/syncer/util.pm
@@ -2,7 +2,7 @@ package Gcis::syncer::util;
 use DateTime;
 use DateTime::Format::ISO8601;
 use base Exporter;
-our @EXPORT_OK = qw/debug info error warning iso_date/;
+our @EXPORT_OK = qw/debug info error warning iso_date pretty_id/;
 our %EXPORT_TAGS = ( log => [qw/debug info error warning/] );
 
 sub debug($)   { Gcis::syncer->logger->debug(@_); }
@@ -15,5 +15,19 @@ sub iso_date {
     my $parsed = DateTime::Format::ISO8601->parse_datetime($dt) or return undef;
     return $parsed->iso8601();
 }
+
+sub pretty_id {
+    my $id = shift or return;
+    $id =~ s[\([^)]+\)][]g;
+    $id = lc $id;
+    $id =~ s/ /-/g;
+    $id =~ s/\//-/g;
+    $id =~ tr/a-z[0-9]_-/_/dc;
+    $id =~ s/-$//;
+    $id =~ s/^-//;
+    die "cannot make id from @_" unless $id;
+    return $id;
+}
+
 1;
 
