e525f1f933a0eb5ad614f235151328c1d8be1514
Brian Duggan <bduggan@usgcrp.gov>
2014-07-31 14:39:14 -0700

    gcmd url

--- lib/Gcis/syncer/gcmd.pm
+++ lib/Gcis/syncer/gcmd.pm
@@ -5,7 +5,7 @@ use Gcis::syncer::util qw/:log iso_date/;
 use base 'Gcis::syncer';
 
 # Only import platforms for which we are missing information
-my $src = q[http://gcmdservices.gsfc.nasa.gov/static/kms/providers/platforms.rdf];
+my $src = q[http://gcmdservices.gsfc.nasa.gov/static/kms/platforms/platforms.rdf];
 
 
 1;
