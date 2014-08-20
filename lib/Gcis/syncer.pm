0c9ac590ce39a9b15072f3b4ee8ab87ec42a6255
Brian Duggan <bduggan@usgcrp.gov>
2014-08-20 14:21:19 -0700

    lookup gcids

--- lib/Gcis/syncer.pm
+++ lib/Gcis/syncer.pm
@@ -1,4 +1,6 @@
 package Gcis::syncer;
+use Gcis::syncer::util qw/:log/;
+use v5.14;
 
 sub new {
     my $s = shift;
@@ -29,6 +31,31 @@ sub logger {
     $_logger = shift;
     warn "unknown logger" unless $_logger->isa("Mojo::Log");
     return $_logger;
+}
+
+sub lookup_or_create_gcid {
+    my $s = shift;
+    my %args = @_;
+    my ($lexicon,$context,$term,$gcid) =
+        @args{qw/lexicon context term gcid/};
+    
+    debug "looking for /lexicon/$lexicon/find/$context/$term";
+    $s->gcis->ua->max_redirects(0);
+    my $existing = $s->gcis->get("/lexicon/$lexicon/find/$context/$term");
+    $s->gcis->ua->max_redirects(5);
+    if ($existing) {
+        my $gcid = $existing->{gcid};
+        return $gcid;
+    }
+    debug "Making a new gcid $lexicon, $context, $term -> $gcid";
+
+    # Make a new one.
+    $gcid =~ m[^/] or die "invalid gcid $gcid";
+
+    return $gcid if $args{dry_run};
+    $s->gcis->post("/lexicon/$lexicon/term/new",
+        { context => $context, term => $term, gcid => $gcid }) or die $s->gcis->error;
+    return $gcid;
 }
 
 
