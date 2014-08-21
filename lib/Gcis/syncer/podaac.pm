0b00431dfdb7957b25d3575966a1806755190119
Brian Duggan <bduggan@usgcrp.gov>
2014-08-20 17:54:15 -0700

    podaac platforms, instruments, lexicon entries

--- lib/Gcis/syncer/podaac.pm
+++ lib/Gcis/syncer/podaac.pm
@@ -9,14 +9,19 @@ use Data::Dumper;
 
 use v5.14;
 our $src = "http://podaac.jpl.nasa.gov/ws/search/dataset/";
-our $meta_src = "http://podaac.jpl.nasa.gov/ws/metadata/dataset/"; # datasetId => PODAAC-TPRGD-NET10, format => gcmd
+our $meta_src = Mojo::URL->new("http://podaac.jpl.nasa.gov/ws/search/dataset/")
+                ->query(format=>'atom', full => 'true', pretty => "true");
+                # shortName => MERGED_TP_J1_OSTM_OST_ALL_V2, pretty => true,
 my $ua  = Mojo::UserAgent->new();
 
 our $map = {
-    identifier  =>  sub { my $dom = shift; "nasa-".(lc $dom->id->text); },
+    identifier  =>  sub { my $dom = shift; my $id = lc $dom->id->text; 
+                          return "nasa-podaac-$id" unless $id =~ /^podaac/;
+                          return "nasa-$id";
+                        },
     name        =>  sub { my $dom = shift; $dom->title->text;         },
-    description =>  sub { my $dom = shift; join "\n", $dom->at('content')->text, "short name : ".$dom->shortName->text;  },
-    native_id   =>  sub { my $dom = shift; $dom->datasetId->text;     },
+    description =>  sub { my $dom = shift; join "\n", $dom->at('content')->text;  },
+    native_id   =>  sub { my $dom = shift; $dom->shortName->text;     },
     url         =>  sub { my $dom = shift; $dom->at('link[title="Dataset Information"]')->attr('href'); },
     lon_min     =>  sub { my $dom = shift; my $w = $dom->at('where') or return undef;
                                            [ split / /, $w->Envelope->lowerCorner->text ]->[0]; },
@@ -30,29 +35,6 @@ our $map = {
     end_time    =>  sub { my $end   = shift->at('end')   or return undef;  return iso_date($end->text)   },
     release_dt  =>  sub { iso_date(shift->updated->text);                                                },
 };
-
-our $rel = [
-    [
-      platforms => sub {
-        my $dataset = shift;
-        my $dif = shift;
-        for ($dif->find("Source_Name > Short_Name")->each) {
-          my $platform_identifier = pretty_id($_->text);
-              debug "dataset ".$dataset->{identifier}." has platform $platform_identifier";
-        }
-      }
-    ],
-    [
-      sensors => sub {
-        my $dataset = shift;
-        my $dif = shift;
-        for ($dif->find("Sensor_Name > Short_Name")->each) {
-          my $sensor_identifier = pretty_id($_->text);
-          debug "dataset ".$dataset->{identifier}." has sensor $sensor_identifier";
-        }
-      },
-    ],
-];
 
 sub sync {
     my $s = shift;
@@ -74,33 +56,43 @@ sub sync {
     while ($more) {
         $more = 0;
         my $tx = $ua->get($url->query([ startIndex => $start_index ]));
-        my $res = $tx->success or die $tx->error;
+        my $res = $tx->success or die $tx->error->{message};
         for my $entry ($res->dom->find('entry')->each) {  ### Processing===[%]       done
             last REQUEST if $limit && $count > $limit;
             $more = 1;
             my %gcis_info = $s->_extract_gcis($entry);
-            next if $gcid && $gcid ne "/dataset/$gcis_info{identifier}";
-            debug "entry #$count : $gcis_info{identifier}";
+
+            # Store mappings to both shortName and id
+            my $dataset_gcid = $s->lookup_or_create_gcid(
+                lexicon => 'podaac', context => 'dataset', term => $gcis_info{native_id},
+                gcid => "/dataset/$gcis_info{identifier}", dry_run => $dry_run,
+            );
+            my $alternate_id = $entry->id->text;
+            $s->lookup_or_create_gcid(
+                lexicon => 'podaac', context => 'datasetId', term => $alternate_id,
+                gcid => $dataset_gcid, dry_run => $dry_run,
+            );
+
+            next if $gcid && $gcid ne $dataset_gcid;
+            debug "entry #$count : $dataset_gcid";
             $count++;
 
             # insert or update
-            my $existing = $c->get("/dataset/$gcis_info{identifier}");
-            my $url = $existing ? "/dataset/$gcis_info{identifier}" : "/dataset";
+            my $existing = $c->get($dataset_gcid);
+            my $url;
+            $url = "/dataset" unless $existing;
             $stats{ ($existing ? "updated" : "created") }++;
             # TODO skip if unchanged
-            debug "sending ".Dumper(\%gcis_info);
+            debug "sending to $url";
+            #debug Dumper(\%gcis_info);
             unless ($dry_run) {
                 $c->post($url => \%gcis_info) or do {
                     error $c->error;
                     warn "error : ".$c->error;
                 };
             }
-            my $dif = $s->_retrieve_dataset_meta($gcis_info{native_id});
-            for my $rel (@$rel) {
-                my ($name,$code) = @$rel;
-                debug "updating $name";
-                $code->(\%gcis_info, $dif);
-            }
+            my $meta = $s->_retrieve_dataset_meta($gcis_info{native_id});
+            $s->_assign_instrument_instances(\%gcis_info, $meta,$dry_run) if $meta;
         }
         $start_index += $per_page;
     }
@@ -112,10 +104,13 @@ sub sync {
 sub _retrieve_dataset_meta {
     my $s = shift;
     my $id = shift;
-    my $url = Mojo::URL->new($meta_src)->query(datasetId => $id, format => 'gcmd');
+    my $url = Mojo::URL->new($meta_src)->clone->query([shortName => $id, format => 'atom']);
     debug "getting $url";
     my $tx = $ua->get($url);
-    my $res = $tx->success or die $tx->error;
+    my $res = $tx->success or do {
+        error $tx->error->{message};
+        return;
+    };
     return $res->dom;
 }
 
@@ -129,4 +124,36 @@ sub _extract_gcis {
     return %new;
 }
 
+sub _assign_instrument_instances {
+  my $s    = shift;
+  my $gcis = shift;
+  my $meta = shift;
+  my $dry_run = shift;
+  my @sources;
+  $meta->find('podaac\:datasetSource')->each( sub {
+      my $e = shift;
+      push @sources, {
+            platform   => $e->sourceShortName->text,
+            instrument => $e->sensorShortName->text };
+    });
+  for my $source (@sources) {
+      debug "Dataset $gcis->{identifier} : $source->{platform} $source->{instrument}";
+      my ($platform_identifier, $instrument_identifier) = map lc, @$source{qw/platform instrument/};
+      # TODO: use lexicons
+      my $instance = $s->gcis->get("/platform/$platform_identifier/instrument/$instrument_identifier") or do {
+          info "Did not find /platform/$platform_identifier/instrument/$instrument_identifier";
+          next;
+      };
+
+      next if $dry_run;
+      info "Assigning instrument and platform";
+      $s->gcis->post("/dataset/rel/$gcis->{identifier}" => {
+              add_instrument_measurement => {
+                  platform_identifier => $platform_identifier,
+                  instrument_identifier => $instrument_identifier
+              }
+          }
+      ) or die $s->gcis->error;
+  }
+}
 
