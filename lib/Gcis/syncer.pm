ef54af123a0cfb581f3233b6154e5b4184f05f60
Brian Duggan <bduggan@usgcrp.gov>
2014-09-05 13:10:01 -0400

    write_resource in syncer.pm

--- lib/Gcis/syncer.pm
+++ lib/Gcis/syncer.pm
@@ -1,27 +1,17 @@
 package Gcis::syncer;
 use Gcis::syncer::util qw/:log/;
+use Mojo::Base qw/-base/;
+use Path::Class qw/dir file/;
+use YAML::XS qw/Dump/;
 use v5.14;
 
-sub new {
-    my $s = shift;
-    my %a = @_;
-    bless \%a, $s;
-}
+has 'stats';
+has 'gcis';
+has 'audit_note';
+has 'base_path' => sub { die "no base extraction path defined"; };
 
 sub sync {
     die "implemented by derived class";
-}
-
-sub stats {
-    shift->{stats};
-}
-
-sub gcis {
-    return shift->{gcis};
-}
-
-sub audit_note {
-    return shift->{audit_note};
 }
 
 my $_logger;
@@ -39,7 +29,7 @@ sub lookup_or_create_gcid {
     my ($lexicon,$context,$term,$gcid) =
         @args{qw/lexicon context term gcid/};
     
-    debug "looking for /lexicon/$lexicon/find/$context/$term";
+    # debug "looking for /lexicon/$lexicon/find/$context/$term";
     $s->gcis->ua->max_redirects(0);
     my $existing = $s->gcis->get("/lexicon/$lexicon/find/$context/$term");
     $s->gcis->ua->max_redirects(5);
@@ -47,16 +37,40 @@ sub lookup_or_create_gcid {
         my $gcid = $existing->{gcid};
         return $gcid;
     }
-    debug "Making a new gcid $lexicon, $context, $term -> $gcid";
 
     # Make a new one.
     $gcid =~ m[^/] or die "invalid gcid $gcid";
 
     return $gcid if $args{dry_run};
+
+    debug "Making a new term $lexicon, $context, $term -> $gcid";
     $s->gcis->post("/lexicon/$lexicon/term/new",
         { context => $context, term => $term, gcid => $gcid }) or die $s->gcis->error;
     return $gcid;
 }
 
+sub write_resource {
+    my $s = shift;
+    my %a = @_;
+    my $dir        = $a{dir}        or die "missing dir";
+    my $identifier = $a{identifier} or die "missing identifier";
+    my $contents   = $a{contents}   or die "missing contents";
+
+    my $top = dir($s->base_path);
+    -d $top or die "cannot open $top";
+    my $sub = $top->subdir($dir);
+    -d $sub or $sub->mkpath;
+    my $file = $sub->file("$identifier.yaml");
+
+    my $new = Dump($contents);
+
+    if (-e $file) {
+        my $existing = scalar $file->slurp;
+        return 1 if $existing eq $new;
+    }
+    debug "writing to $file";
+    $file->spew($new);
+    return 1;
+}
 
 1;
