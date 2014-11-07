a0299480f05add1edc05df33e52fb8ead3983cfb
Robert Wolfe <rewolfe@usgcrp.gov>
2014-11-07 12:01:39 -0700

    Added sync of volume and journal title

--- lib/Gcis/syncer/article.pm
+++ lib/Gcis/syncer/article.pm
@@ -23,35 +23,53 @@ sub _set_title {
     my $article = shift;
     my $title = shift;
     return 0 if $article->{title} eq $title;
-    my $return_value = 1;
-    if (very_different($article->{title}, $title)) {
-        info "old title : $article->{title}";
-        info "new title : $title";
-        $return_value = 2;
-    }
+    my $return_value = very_different($article->{title}, $title) ? 2 : 1;
+    info "old title : $article->{title}";
+    info "new title : $title";
     $article->{title} = $title;
     return $return_value;
 }
 
-# return 1 if year of article is one less (don't update year)
+# return 1 if year of article is one more (don't update year)
 # return 2 if article year is more different
 sub _set_year {
     my $s = shift;
     my $article = shift;
     my $year = shift;
     return 0 if $article->{year} eq $year;
-    debug "year : changing $article->{year} to $year";
-    my $return_value = 1;
-    if ($article->{year} != ($year - 1)) {
-        info "old year : $article->{year}";
-        info "new year : $year";
-        $return_value = 2;
-        $article->{year} = $year;
-    }
+    if ($article->{year} == ($year + 1)) {
+      info "article year is one more than authority - no update";
+      return 1;
+    } 
+    info "old year : $article->{year}";
+    info "new year : $year";
+    return 2;
+}
+
+sub _set_vol {
+    my $s = shift;
+    my $article = shift;
+    my $vol = shift;
+    return 0 if $article->{journal_vol} == $vol;
+    info "old volume : $article->{journal_vol}";
+    info "new volume : $vol";
+    $article->{journal_vol} = $vol;
     return 1;
 }
 
-#is $article->{journal_vol},    $crossref->{volume}, "volume";
+# return 1 if changed
+# return 2 if very different
+sub _set_journal_title {
+    my $s = shift;
+    my $journal = shift;
+    my $title = shift;
+    return 0 if $journal->{title} eq $title;
+    info "old journal title : $journal->{title}";
+    info "new journal title : $title";
+    my $return_value = very_different($journal->{title}, $title) ? 2 : 1;
+    $journal->{title} = $title;
+    return $return_value;
+}
 
 $|=1;
 sub sync {
@@ -65,13 +83,17 @@ sub sync {
 
     my $d = Gcis::Client->new->accept("application/vnd.citationstyles.csl+json;q=0.5")
              ->url("http://dx.doi.org");
+    my $r = Gcis::Client->new->accept("application/json;q=0.5")
+             ->url("http://api.crossref.org");
+
     $d->logger($c->logger);
+    $r->logger($c->logger);
     my @articles;
     if ($gcid) {
        @articles = ( $c->get($gcid) );
     } else {
        @articles = @{ $c->get('/article?all=1') };
-   }
+    }
     my %stats;
     my $i = 0;
     debug "debugging output enabled";
@@ -79,7 +101,8 @@ sub sync {
         debug "article $art->{identifier} :";
         last if $limit && $i++ > $limit;
         my $uri = $art->{uri} or die "no uri";
-        my $article = $c->get_form($art) or die "could not get form : ".Dumper($art);
+        my $article = $c->get_form($art) or 
+            die "could not get article form : ".Dumper($art);
         my $doi = $article->{doi} or do {
             debug "no doi for ".Dumper($article);
             next;
@@ -88,20 +111,27 @@ sub sync {
             warning "No info from crossref for $doi";
             next;
         };
-        my $changed;
+        my $jou = $c->get("/journal/$article->{journal_identifier}") or 
+            die "could not get journal : $article->{journal_identifer}";
+        my $journal = $c->get_form($jou) or 
+            die "could not get journal form : ".Dumper($jou);
+
+        my %changed;
         my $how = "";
+
         if ($crossref->{title}) {
             if (my $title_change = $s->_set_title($article, $crossref->{title})) {
-                $changed = 1;
+                $changed{article} = 1;
                 $stats{title_change}++;
                 $how = $title_change == 1 ? "title touch-up" : "major title change";
             }
         } else {
             warning "no title in crossref for $doi";
         }
+
         if (my $year = $crossref->{issued}{'date-parts'}[0][0]) {
             if (my $year_change = $s->_set_year($article, $year) > 1) {
-                $changed = 1;
+                $changed{article} = 1;
                 $stats{year_changed}++;
                 $how .= ", " if $how;
                 $how .= $year_change == 1 ? "year touch-up" : "major year change";
@@ -109,8 +139,45 @@ sub sync {
         } else {
             warning "No year in crossref for $doi";
         }
+
+        if (my $vol = $crossref->{volume}) {
+            if (my $vol_change = $s->_set_vol($article, $vol)) {
+                $changed{article} = 1;
+                $stats{vol_changed}++;
+                $how .= ", " if $how;
+                $how .= "volume change";
+            }
+        } else {
+            warning "No volume in crossref for $doi";
+        }
+
+        {
+            my $issn = $crossref->{ISSN}[0];
+            unless ($issn) {
+                warning "No ISSN in crossref for $doi";
+                next;
+            }
+            my $crossref_journal = $r->get("/journals/issn:$issn");
+            unless ($crossref_journal) {
+                warning "No journal in crossref for $doi";
+                next;
+            }
+            my $journal_title = $crossref_journal->{message}{title};
+            unless ($journal_title) {
+                warning "No journal title in crossref for $doi";
+                next;
+            }
+            if (my $journal_title_change = $s->_set_journal_title($journal, $journal_title)) {
+                $changed{journal} = 1;
+                $stats{journal_title_changed}++;
+                $how .= ", " if $how;
+                $how .= $journal_title_change == 1 ? "journal title touch-up" : 
+                                                     "journal major title change";
+            }
+        }
+
         if ($dry_run) {
-            if ($changed) {
+            if ($changed{article} || $changed{journal}) {
                 info "ready to save http://dx.doi.org/$doi";
                 info "$uri : update ($how)";
             } else {
@@ -118,13 +185,18 @@ sub sync {
             }
             next;
         }
-        unless ($changed) {
+        unless ($changed{article} || $changed{journal}) {
             debug "$uri : skip";
             $stats{skip}++;
+            next;
         }
-        next unless $changed;
         info "$uri : update ($how)";
-        $c->post( "/article/$article->{identifier}" => $article) or error $c->error;
+        if ($changed{article}) {
+            $c->post( "/article/$article->{identifier}" => $article) or error $c->error;
+        }
+        if ($changed{journal}) {
+            $c->post( "/journal/$article->{journal_identifier}" => $journal) or errror $c->error;
+        }
     }
     $s->{stats} = \%stats;
     return;
