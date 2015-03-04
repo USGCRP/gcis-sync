50195a082707a88a50d809c1f1a9ed7f1e79d562
Brian Duggan <bduggan@usgcrp.gov>
2015-03-04 15:53:38 -0500

    updates to echo

--- lib/Gcis/syncer/echo.pm
+++ lib/Gcis/syncer/echo.pm
@@ -11,37 +11,52 @@ use v5.14;
 
 our $src = qw[https://api.echo.nasa.gov/catalog-rest/echo_catalog/datasets.echo10];
 our $map = {
-      identifier  => sub { "nasa-echo-" . (lc shift->attr('echo_dataset_id')); },
-      name        => "collection > datasetid",
-      description => "collection > description",
-      native_id   => "collection > shortname",
+      identifier  => sub {
+          my $dom = shift;
+          my $archive_center = $dom->at('ArchiveCenter') or return;
+          $archive_center = lc $archive_center->text;
+          my $native_id = lc $dom->at('Collection > ShortName')->text;
+          $archive_center .= 'daac' unless $archive_center =~ /daac$/;
+          $native_id =~ s/^$archive_center-//;
+          return "nasa-".(lc $archive_center)."-$native_id";
+      },
+      name        => "Collection > DataSetId",
+      description => "Collection > Description",
+      native_id   => "Collection > ShortName",
       url         => [
-                       "result > collection > onlineaccessurls > onlineaccessurl > url",
-                       sub { "http://reverb.echo.nasa.gov/reverb?selected=".shift->attr('echo_dataset_id'); }
-                   ],
-      release_dt => "collection > inserttime",
-      lat_min    => "collection > spatial > horizontalspatialdomain > geometry > boundingrectangle > southboundingcoordinate",
-      lat_max    => "collection > spatial > horizontalspatialdomain > geometry > boundingrectangle > northboundingcoordinate",
-      lon_min    => "collection > spatial > horizontalspatialdomain > geometry > boundingrectangle > westboundingcoordinate",
-      lon_max    => "collection > spatial > horizontalspatialdomain > geometry > boundingrectangle > eastboundingcoordinate",
+                       "OnlineResources > OnlineResource > URL",
+                       "OnlineAccessURLs > OnlineAccessURL > URL",
+                       sub { warn "no url for ".shift->attr('echo_dataset_id'); return undef; },
+                      ],
+      release_dt => "Collection > InsertTime",
+      lat_min    => "Collection > Spatial > HorizontalSpatialDomain > Geometry > BoundingRectangle > SouthBoundingCoordinate",
+      lat_max    => "Collection > Spatial > HorizontalSpatialDomain > Geometry > BoundingRectangle > NorthBoundingCoordinate",
+      lon_min    => "Collection > Spatial > HorizontalSpatialDomain > Geometry > BoundingRectangle > WestBoundingCoordinate",
+      lon_max    => "Collection > Spatial > HorizontalSpatialDomain > Geometry > BoundingRectangle > EastBoundingCoordinate",
       start_time => [
-                     "collection > temporal > rangedatetime > beginningdatetime",
-                     "collection > temporal > rangedatetime > singledatetime",
+                     "Collection > Temporal > RangeDateTime > BeginningDateTime",
+                     "Collection > Temporal > RangeDateTime > SingleDateTime",
                     ],
       end_time   => [
-                     "collection > temporal > rangedatetime > endingdatetime",
-                     "collection > temporal > rangedatetime > singledatetime",
+                     "Collection > Temporal > RangeDateTime > EndingDateTime",
+                     "Collection > Temporal > RangeDateTime > SingleDateTime",
                     ],
       doi => sub {
           my $dom = shift;
-          my $identifier = $dom->at("collection > shortname")->text;
+          my $identifier = $dom->at("Collection > ShortName")->text;
+          die "no shortname in ".$dom->to_string unless $identifier;
           $identifier =~ /^doi:(.*)$/ and return $1;
-          for my $url ($dom->find("onlineresource > url")->each) {
+          for my $url ($dom->find("OnlineResource > URL")->each) {
               $url->text =~ m[dx.doi.org/(.*)$] and return $1;
           }
           return undef;
       },
 };
+
+our $daacs = {
+    nsidc => '/organization/national-snow-ice-data-center',
+};
+
 sub sync {
     my $s = shift;
     my %a = @_;
@@ -65,17 +80,25 @@ sub sync {
         $more = 0;
         debug "getting $url (page $page)";
         my $tx = $ua->get($url->query([ page_num => $page++ ]));
-        my $res = $tx->success or die $tx->error;
+        my $res = $tx->success or die "$url : ".Dumper($tx->error);
         for my $entry ($res->dom->find('result')->each) {  ### Processing===[%]       done
+            $more = 1;
+            my $daac = $entry->at('ArchiveCenter') or do {
+                info "no archivecenter for ".$entry->at('ShortName')->text;
+                next;
+            };
+            $daac = lc $daac->text;
+            next unless $daac eq 'nsidc';
+            debug "archive center : $daac";
             last REQUEST if $limit && $count++ > $limit;
             my %gcis_info = $s->_extract_gcis($entry);
-            $more = 1;
             next if $gcid && $gcid ne "/dataset/$gcis_info{identifier}";
 
             # insert or update
             my $existing = $c->get("/dataset/$gcis_info{identifier}");
             my $url = $existing ? "/dataset/$gcis_info{identifier}" : "/dataset";
             $stats{ ($existing ? "updated" : "created") }++;
+            debug "xml data : ".$entry->to_string;
             debug "gcis data : ".Dumper(\%gcis_info);
             if ($dry_run) {
                 info "ready to POST to $url";
@@ -86,6 +109,12 @@ sub sync {
                 error $c->error;
                 die "bailing out, error : ".$c->error;
             };
+            if (my $archive = $daacs->{$daac}) {
+                my $id = $gcis_info{identifier} or die "no id in ".Dumper(\%gcis_info);
+                $s->_assign_contributors( "/dataset/$id", \%gcis_info, $archive, $dry_run);
+            } else {
+                die "contributor not found : $daac";
+            }
         }
     }
 
@@ -103,7 +132,7 @@ sub extract {
         for (@$e) {
             my $val = extract($_, $dom);
             $val = $val->text if ref($val) eq 'Mojo::DOM';
-            return $val if $val;
+            return $val if $val && $val !~ /^\s+$/;
         }
     };
     return undef;
@@ -126,12 +155,23 @@ sub _extract_gcis {
     my %new = map {
              $_ => fmt(extract($map->{$_}, $dom), $_)
          } keys %$map;
-    debug "platform : ".fmt(extract("platforms > platform > shortname", $dom));
-    debug "Instrument : ".fmt(extract("instrument > shortname", $dom));
-    debug "sensor : ".fmt(extract("sensor > shortname", $dom));
+    debug "platform : ".fmt(extract("Platforms > Platforms > ShortName", $dom));
+    debug "Instrument : ".fmt(extract("Instrument > ShortName", $dom));
+    debug "sensor : ".fmt(extract("Sensor > ShortName", $dom));
     $count++;
     debug "extracting entry $count : $new{identifier} : $new{native_id}";
     return %new;
 }
 
+sub _assign_contributors {
+    my $s = shift;
+    my ($gcid, $info, $data_archive, $dry_run ) = @_;
+    die "no gcid" unless $gcid;
+    return if $dry_run;
+    my $contribs_url = $gcid =~ s[dataset/][dataset/contributors/]r;
+    $s->gcis->post($contribs_url => {
+                organization_identifier => $data_archive,
+                role => 'data_archive'
+        }) or error $s->gcis->error;
+}
 
